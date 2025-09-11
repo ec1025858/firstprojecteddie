eda64c241673bf31c71bc985aaf6e9467a6d1e5a
ec1025858 <ec1025858@otc.edu>
2025-09-11 09:38:41 -0500

    Add files via upload

---
+++ Program #1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What's your first name?");
+var F = Console.ReadLine();
+Console.WriteLine("What's your last name?");
+var L = Console.ReadLine();
+Console.WriteLine($"Greetings {F} {L}!");
+Console.WriteLine("Type a number.");
+string x = Console.ReadLine() + "";
+int X = Convert.ToInt32(x);
+Console.WriteLine("Type another number.");
+string y = Console.ReadLine() + "";
+int Y = Convert.ToInt32(y);
+Console.WriteLine("Type in one last number");
+string z = Console.ReadLine() + "";
+int Z = Convert.ToInt32(z);
+int a = X + Y + Z;
+Console.WriteLine(a);
+Console.WriteLine("" + (Y - X));
+int b = X * Z;
+Console.WriteLine("" + (a / Y));
+Console.WriteLine("" + (X % b));
