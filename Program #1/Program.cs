14391e92bf52634a6a313d60b82c8676664519db
ec1025858 <ec1025858@otc.edu>
2025-09-11 09:49:01 -0500

    Update Program.cs

--- Program #1/Program.cs
+++ Program #1/Program.cs
@@ -4,6 +4,7 @@ var F = Console.ReadLine();
 Console.WriteLine("What's your last name?");
 var L = Console.ReadLine();
 Console.WriteLine($"Greetings {F} {L}!");
+//The code asks the user their name, then greets them.
 Console.WriteLine("Type a number.");
 string x = Console.ReadLine() + "";
 int X = Convert.ToInt32(x);
@@ -13,9 +14,11 @@ int Y = Convert.ToInt32(y);
 Console.WriteLine("Type in one last number");
 string z = Console.ReadLine() + "";
 int Z = Convert.ToInt32(z);
+//The code asks the user to type three different numbers.
 int a = X + Y + Z;
 Console.WriteLine(a);
 Console.WriteLine("" + (Y - X));
 int b = X * Z;
 Console.WriteLine("" + (a / Y));
 Console.WriteLine("" + (X % b));
+//The code uses the numbers that it asked user for to do a series of equations.
